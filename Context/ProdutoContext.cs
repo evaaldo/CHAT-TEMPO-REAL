49e0afa8b707e83e50c9e070f930ed42dede1815
Evaldo Junior <evaaldormj@gmail.com>
2023-11-12 15:08:23 -0300

    Feat: Criando contexto

---
+++ Context/ProdutoContext.cs
@@ -0,0 +1,15 @@
+using GerenciamentoProdutos.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoProdutos.Context
+{
+    public class ProdutoContext : DbContext
+    {
+        public ProdutoContext(DbContextOptions<ProdutoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
