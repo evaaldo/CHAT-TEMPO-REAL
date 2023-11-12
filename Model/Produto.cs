44d81ccd609af1a3eaf3171721e1b0e5eaa50f55
Evaldo Junior <evaaldormj@gmail.com>
2023-11-12 15:02:46 -0300

    Feat: Model Produto

---
+++ Model/Produto.cs
@@ -0,0 +1,9 @@
+namespace GerenciamentoProdutos.Model
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string? Nome { get; set; }
+        public long Preco { get; set; }
+    }
+}
